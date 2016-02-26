eec834328b289ee1c46a8ad7ac7a3bac88dfd482
Samuel Shepard <vfn4@cdc.gov>
2016-02-26 11:55:01 -0500

    Added amino acid MSA editing. Creates an editable AAtoNT file and/or splices now.

--- spliceAlignment.pl
+++ spliceAlignment.pl
@@ -70,18 +70,26 @@
 
 use File::Basename;
 use Getopt::Long;
-GetOptions(	'protein-seq|A=s'=> \$aaFile, 'fill-missing|F' => \$fillMissing
+GetOptions(	'amino-acid-msa|A=s'=> \$aaFile, 'fill-missing|F' => \$fillMissing, 'prefix|P=s' => \$prefix, 'splice-now|S' => \$spliceNow
 		);
 
 if ( scalar(@ARGV) != 2 ) {
 	$message = "Usage:\n\tperl $0 <diced> <miniNT> [options]\n";
-	$message .= "\t\t-A|--protein-seq <FILE>\t\tAmino acid alignment.\n";
 	$message .= "\t\t-F|--fill-missing\t\tFill missing data with gaps rather than excluding.\n";
+	$message .= "\t\t-A|--amino-acid-msa <FILE>\tAmino acid alignment.\n";
+	$message .= "\t\t-P|--prefix <STR>\t\tPrefix used for AAtoNT file output.\n";
+	$message .= "\t\t-S|--splice-now\t\t\tOutput the splice without further editing of the AAtoNT file.\n";
 	die($message."\n");
 }
 
 open(DICE,'<',$ARGV[0]) or die("Cannot open $ARGV[0] for reading.\n");
 open(NTS,'<',$ARGV[1]) or die("Cannot open $ARGV[1] for reading.\n");
+
+if ( !defined($prefix) ) {
+	$prefix = dirname($ARGV[1]);
+	$prefix .= '/'.basename($ARGV[1],'.miniNT.fasta');
+}
+
 
 $/ = ">";
 while($record = <NTS>) {
@@ -94,14 +102,15 @@ while($record = <NTS>) {
 	if ( $length == 0 ) { next; }
 	($cluster,$count) = split('\|',$header);
 	$ntByCluster{$cluster} = $sequence;
+	$countByCluster{$cluster} = $count;
 	$fillLength = $length;
 }
 close(NTS);
 
 if ( defined($aaFile) ) {
 	open(AA,'<',$aaFile) or die("Cannot open $aaFile for reading.\n");
-	$/ = '>';
-	while($record = <NTS>) {
+	$/ = '>'; %aaByCluster = ();
+	while($record = <AA>) {
 		chomp($record);
 		@lines = split(/\r\n|\n|\r/, $record);
 		$header = shift(@lines);
@@ -117,6 +126,16 @@ if ( defined($aaFile) ) {
 		$fillLengthAA = $length;
 	}
 	close(AA);
+	open(AATONT,'>',$prefix.'.AAtoNT.fasta') or die("Cannot open $prefix.'.AAtoNT.fasta' for writing.\n");
+	foreach $cluster ( keys(%ntByCluster) ) {
+		$ntByCluster{$cluster} = realignByAA($ntByCluster{$cluster},$aaByCluster{$cluster});
+		print AATONT '>',$cluster,'|',$countByCluster{$cluster},"\n",$ntByCluster{$cluster},"\n";
+	}
+	close(AATONT);
+	if ( !defined($spliceNow) ) {
+		print STDERR "Created '$prefix.AAtoNT.fasta' for editing.\n";
+		exit 0;
+	}
 }
 
 $/ = "\n";
@@ -126,9 +145,6 @@ while($line = <DICE> ) {
 	($header,$left,$cluster,$right) = split("\t",$line);
 	if ( defined($ntByCluster{$cluster}) ) {
 		$mid = $ntByCluster{$cluster};
-		if ( defined($aaByCluster{$cluster}) ) {
-			$mid = realignByAA($fillLength,$fillLengthAA,$mid,$aaByCluster{$cluster});	
-		}
 		print '>',$header,"\n",$left,$mid,$right,"\n";
 	} elsif( $fillMissing ) {
 		print '>',$header,"\n",$left,('-'x$fillLength),$right,"\n";
@@ -136,25 +152,50 @@ while($line = <DICE> ) {
 }
 close(DICE);
 
+sub realignByAA($$) {
+	my ($ntS,$aaS) = (uc($_[0]),uc($_[1]));
+	my ($r,$s,$codons) = ('','','');
+	my ($gap,$i) = (0,0);
+	my @codons = ();
+	my @residues = ();
 
-sub realignByAA($$$$) {
-	my ($ntL,$aaL,$ntS,$aaS) = @_;
-	# rewrite code
-	return $ntS;
-}
-
-sub translate($) {
-	my $nt = uc($_[0]);
-	my $aa = '';
-	my $i = 0;
-	my $codon = '';
-	for($i=0;$i<length($nt);$i+=3) {
-		$codon = substr($nt,$i,3);
+	$ntS =~ tr/-//d;
+	my $ntL = length($ntS);
+	my $gap = 3 - ($ntL % 3);
+	if ( $gap != 3 ) {
+		$ntS .= ('-'x$gap);
+		$ntL += $gap;
+	}
+	for($i=0;$i<$ntL;$i+=3) {
+		$codon = substr($ntS,$i,3);
 		if ( !defined($gc{$codon}) ) {
-			$aa .= '?';
+			$r = '?';
 		} else {
-			$aa .= $gc{$codon};
+			$r = $gc{$codon};
+		}
+		push(@codons,$codon);
+		push(@residues,$r);
+	}
+
+	$i = 0;
+	for $r ( split('',$aaS) ) {
+		if ( $r eq '-' ) {
+			$s .= '---';
+		} elsif ( $r eq $residues[$i] ) {
+			$s .= $codons[$i];
+			$i++;
+		} else {
+			$i++;
+			while($i <= $#residues) {
+				if ( $r eq $residues[$i] ) {
+					$s .= $codons[$i];
+					$i++;
+					last;
+				} else {
+					$i++;
+				}
+			}
 		}
 	}
-	return $aa;
+	return lc($s);
 }
