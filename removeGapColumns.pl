b541b147546419d44b281ac26785e90ff756b7e8
Samuel Shepard <vfn4@cdc.gov>
2020-04-27 08:59:49 -0400

    Fixed -R, added -A to removeGapColumns

--- removeGapColumns.pl
+++ removeGapColumns.pl
@@ -25,7 +25,7 @@
 
 use Getopt::Long;
 GetOptions( 	'display-k-tons|D=i'=> \$ktons, 'remove-k-tons|R' => \$removeKtons, 'tab-formatted|T' => \$tabDelimited,
-		'show-column-support|S' => \$showSupport
+		'show-column-support|S' => \$showSupport, 'a2m-insertions-only|A' => \$a2mInsertionsOnly
 		);
 
 
@@ -43,6 +43,9 @@ open(IN, '<', $ARGV[0]) or die("Cannot open $ARGV[0].\n");
 
 # PROCESS fasta data
 $/ = ">"; $i = 0;
+my $first = 1;
+my @allowed = ();
+my $del = '-';
 while($record = <IN> ) {
 	chomp($record);
 	@lines = split(/\r\n|\n|\r/, $record);
@@ -54,20 +57,39 @@ while($record = <IN> ) {
 		next;
 	}
 
-	# unify gap characters
-	$sequence[$i] =~ tr/:~./-/;
 
-	$R = index($sequence[$i],'-', 0);
-	while ( $R != -1 ) {
-		$gaps{$R}++;
-		$R = index($sequence[$i],'-',$R+1);	
+	if ( defined($a2mInsertionsOnly) ) {
+		if ( $first ) {
+			my @C = split('',$sequence[$i]);
+			foreach my $j ( 0 .. $#C ) {
+				if ( $C[$j] =~ /[a-z.]/ ) {
+					push(@allowed,$j);
+				}
+			}	
+			$first = 0;	
+			$del = '.';
+		}
+
+		foreach my $j ( @allowed ) {
+			if ( substr($sequence[$i],$j,1) eq '.' ) {
+				$gaps{$j}++;
+			}
+		}
+	} else {
+		# unify gap characters
+		$sequence[$i] =~ tr/:~./-/;
+
+		$R = index($sequence[$i],'-', 0);
+		while ( $R != -1 ) {
+			$gaps{$R}++;
+			$R = index($sequence[$i],'-',$R+1);	
+		}
 	}
 	$i++;
 }
 close(IN);
 $totalLength = length($sequence[0]);
 $numSeqs = $i;
-
 
 if ( $ktons > 0 ) {
 
@@ -81,7 +103,7 @@ if ( $ktons > 0 ) {
 			$i = 0;
 			while ( $support > 0 && $i < $numSeqs ) {
 				$base = substr($sequence[$i], $g, 1);
-				if ( '-' ne $base ) {
+				if ( $del ne $base ) {
 					print STDERR "\t",uc($base)," ",$header[$i],"\n";
 					if ( $tabDelimited ) {
 						print $g,"\t",$support,"\t",uc($base),"\t",$header[$i],"\n";
@@ -90,6 +112,7 @@ if ( $ktons > 0 ) {
 				}
 				$i++;
 			}
+		} else {
 			delete($gaps{$g});
 		}
 	}
