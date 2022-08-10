b6a50c3fe0d329cf47ab4a3f764daec0934811a7
Samuel Shepard <vfn4@cdc.gov>
2022-08-10 10:27:21 -0400

    Clean up stats scripts.

--- displayStats.pl
+++ displayStats.pl
@@ -1,20 +1,49 @@
 #!/usr/bin/env perl
-# Samuel Shepard - 2019.03.11
-# Version 1.0
-# Codon corrects sequences from user-supplied statistics.
+# Filename:         displayStats
+# Description:      Displays the codon weight matrix (frequency stats).
+#
+# Date dedicated:   2022-08-10
+# Author:           Samuel S. Shepard, Centers for Disease Control and Prevention
+#
+# Citation:         Unpublished
+#
+# =============================================================================
+#
+#                            PUBLIC DOMAIN NOTICE
+#
+#  This source code file or script constitutes a work of the United States
+#  Government and is not subject to domestic copyright protection under 17 USC §
+#  105. This file is in the public domain within the United States, and
+#  copyright and related rights in the work worldwide are waived through the CC0
+#  1.0 Universal public domain dedication:
+#  https://creativecommons.org/publicdomain/zero/1.0/
+#
+#  The material embodied in this software is provided to you "as-is" and without
+#  warranty of any kind, express, implied or otherwise, including without
+#  limitation, any warranty of fitness for a particular purpose. In no event
+#  shall the Centers for Disease Control and Prevention (CDC) or the United
+#  States (U.S.) government be liable to you or anyone else for any direct,
+#  special, incidental, indirect or consequential damages of any kind, or any
+#  damages whatsoever, including without limitation, loss of profit, loss of
+#  use, savings or revenue, or the claims of third parties, whether or not CDC
+#  or the U.S. government has been advised of the possibility of such loss,
+#  however caused and on any theory of liability, arising out of or in
+#  connection with the possession, use or performance of this software.
+#
+#  Please provide appropriate attribution in any work or product based on this
+#  material.
 
 use strict;
 use warnings;
 use Storable;
 use Getopt::Long;
+use English qw( -no_match_vars);
 
 my $verbose;
 GetOptions( 'verbose|V' => \$verbose );
 
 if ( scalar(@ARGV) != 1 ) {
-    my $message = "Usage:\n\tperl $0 <STATS.sto> [options]\n";
-    $message .= "\t\t--verbose|V\tMake display verbose.\n";
-    die( $message . "\n" );
+    die( "Usage:\n\tperl $PROGRAM_NAME <STATS.sto> [options]\n" . "\t\t--verbose|V\tMake display verbose.\n\n" );
 }
 
 # Obtain alignment-specific codon statistics if available
@@ -23,7 +52,7 @@ my %codonStats = %{ retrieve($stats) } or die("Cannot open statistics file '$sta
 
 if ( defined($verbose) ) {
     my @keys = keys(%codonStats);
-    if ( $keys[0] =~ /^\d+$/ ) {
+    if ( $keys[0] =~ /^\d+$/smx ) {
         foreach my $p ( sort { $a <=> $b } @keys ) {
             foreach my $c ( sort { $codonStats{$p}{$b} <=> $codonStats{$p}{$a} } keys( %{ $codonStats{$p} } ) ) {
                 print STDOUT $p, "\t", $c, "\t", $codonStats{$p}{$c}, "\n";
@@ -44,7 +73,7 @@ if ( defined($verbose) ) {
 } else {
     my $maxC = 0;
     my @keys = keys(%codonStats);
-    if ( $keys[0] =~ /^\d+$/ ) {
+    if ( $keys[0] =~ /^\d+$/smx ) {
         @keys = sort { $a <=> $b } @keys;
         print STDOUT "Number of Codons\tMaximum Codon Depth\n";
         foreach my $p (@keys) {
--- storeStats.pl
+++ storeStats.pl
@@ -1,9 +1,45 @@
 #!/usr/bin/env perl
-# Samuel Shepard - 3.8.2019
-# Stores alignment statistics for other uses.
+# Filename:         storeStats
+# Description:      Computes & stores the codon weight matrix from an MPSA.
+#
+# Date dedicated:   2022-08-10
+# Author:           Samuel S. Shepard, Centers for Disease Control and Prevention
+#
+# Citation:         Unpublished
+#
+# =============================================================================
+#
+#                            PUBLIC DOMAIN NOTICE
+#
+#  This source code file or script constitutes a work of the United States
+#  Government and is not subject to domestic copyright protection under 17 USC §
+#  105. This file is in the public domain within the United States, and
+#  copyright and related rights in the work worldwide are waived through the CC0
+#  1.0 Universal public domain dedication:
+#  https://creativecommons.org/publicdomain/zero/1.0/
+#
+#  The material embodied in this software is provided to you "as-is" and without
+#  warranty of any kind, express, implied or otherwise, including without
+#  limitation, any warranty of fitness for a particular purpose. In no event
+#  shall the Centers for Disease Control and Prevention (CDC) or the United
+#  States (U.S.) government be liable to you or anyone else for any direct,
+#  special, incidental, indirect or consequential damages of any kind, or any
+#  damages whatsoever, including without limitation, loss of profit, loss of
+#  use, savings or revenue, or the claims of third parties, whether or not CDC
+#  or the U.S. government has been advised of the possibility of such loss,
+#  however caused and on any theory of liability, arising out of or in
+#  connection with the possession, use or performance of this software.
+#
+#  Please provide appropriate attribution in any work or product based on this
+#  material.
 
+use strict;
+use warnings;
+use English qw( -no_match_vars);
 use Storable qw(store_fd store);
 use Getopt::Long;
+
+my ( $outfile, $delim, $fieldSet );
 GetOptions(
             'output|O=s' => \$outfile,
             'delim|D=s'  => \$delim,
@@ -11,95 +47,102 @@ GetOptions(
 );
 
 if ( -t STDIN && scalar(@ARGV) != 1 ) {
-    $message = "Usage:\n\tperl $0 <nts.fasta> [options]\n";
-    $message .= "\t\t--output|-O <file.sto>\tOutput file for storable object. Default: STDOUT\n";
-    $message .= "\t\t--delim|-D <CHAR>\tDelimiter for header fields. Default delim is '|'.\n";
-    $message .= "\t\t--field|-F <STR>\tComma-delimited set of fields to use for group. Default: no group.\n";
-    die( $message . "\n" );
+    die(   "Usage:\n\tperl $PROGRAM_NAME <nts.fasta> [options]\n"
+         . "\t\t--output|-O <file.sto>\tOutput file for storable object. Default: STDOUT\n"
+         . "\t\t--delim|-D <CHAR>\tDelimiter for header fields. Default delim is '|'.\n"
+         . "\t\t--field|-F <STR>\tComma-delimited set of fields to use for group. Default: no group.\n"
+         . "\n" );
 }
 
-$numberSelected = 0;
-if ( defined($fieldSet) ) {
+my $numberSelected = 0;
+my @fields         = ();
+if ( defined $fieldSet ) {
     @fields         = split( ',', $fieldSet );
     $numberSelected = scalar(@fields);
-    foreach $x (@fields) {
+    foreach my $x (@fields) {
         if ( $x == 0 ) {
-            die("$0 ERROR: field must be specified.\n");
+            die("$PROGRAM_NAME ERROR: field must be specified.\n");
         } elsif ( $x < 0 ) {
-            die("$0 ERROR: field must be a positive number.\n");
+            die("$PROGRAM_NAME ERROR: field must be a positive number.\n");
         }
     }
-    for ( $x = 0; $x < $numberSelected; $x++ ) { $fields[$x]--; }
+    foreach my $x ( 0 .. ( $numberSelected - 1 ) ) {
+        $fields[$x]--;
+    }
 }
 
-if ( !defined($delim) ) {
+if ( !defined $delim ) {
     $delim = '|';
-} elsif ( $delim eq '' ) {
-    die("$0 ERROR: No delimiter argument detected.\n");
+} elsif ( $delim eq q{} ) {
+    die("$PROGRAM_NAME ERROR: No delimiter argument detected.\n");
 } elsif ( length($delim) > 1 ) {
-    die("$0 ERROR: single character delimiter expected instead of '$delim'.\n");
+    die("$PROGRAM_NAME ERROR: single character delimiter expected instead of '$delim'.\n");
 }
 
-@patterns = ();
-$/        = ">";
-while ( $record = <> ) {
-    chomp($record);
-    @lines    = split( /\r\n|\n|\r/, $record );
-    $id       = shift(@lines);
-    $sequence = lc( join( '', @lines ) );
-    $length   = length($sequence);
+my %counts = ();
+local $RS = ">";
+while ( my $fasta_record = <> ) {
+    chomp($fasta_record);
+    my @lines    = split( /\r\n|\n|\r/smx, $fasta_record );
+    my $id       = shift(@lines);
+    my $sequence = lc( join( q{}, @lines ) );
+    my $length   = length($sequence);
 
     if ( $length == 0 ) {
         next;
     } elsif ( $length % 3 != 0 ) {
-        die("$PROG:\tNot a codon alignment! See:\n\t$firstFile\n");
+        die("$PROGRAM_NAME:\tNot a codon alignment, length $length not divisible by 3!\n");
     } else {
         if ( $numberSelected > 0 ) {
-            @values      = split( /\Q$delim\E/, $id );
-            $numberFound = scalar(@values);
+            my @values      = split( /\Q$delim\E/smx, $id );
+            my $numberFound = scalar(@values);
             if ( $numberFound < $numberSelected ) {
-                die("$0 ERROR: non-existant field specified. Wanted $numberSelected but found $numberFound\n");
+                die("$PROGRAM_NAME ERROR: non-existant field specified. Wanted $numberSelected but found $numberFound\n");
             }
             $id = join( $delim, ( @values[@fields] ) );
         }
 
-        $group = $numberSelected > 0 ? $id : '__NIL__';
+        my $group = $numberSelected > 0 ? $id : '__NIL__';
         $counts{$group}{$sequence}++;
     }
 }
 
-@groups    = keys(%counts);
-%codonData = ();
-if ( scalar(@groups) == 1 && $groups[0] eq '__NIL__' ) {
-    $group    = '__NIL__';
-    @patterns = keys( %{ $counts{$group} } );
+my @groups    = keys(%counts);
+my %codonData = ();
+if ( scalar @groups == 1 && $groups[0] eq '__NIL__' ) {
+    my $group    = '__NIL__';
+    my @patterns = keys( %{ $counts{$group} } );
     foreach my $pattern (@patterns) {
-        $length = length($pattern);
-        $count  = $counts{$group}{$pattern};
-        for ( $pos = 0; $pos < $length; $pos += 3 ) {
+        my $length = length($pattern);
+        my $count  = $counts{$group}{$pattern};
+
+        ## no critic (ControlStructures::ProhibitCStyleForLoops)
+        for ( my $pos = 0; $pos < $length; $pos += 3 ) {
 
             # zero based codon number
-            $codonNumber = int( $pos / 3 );
-            $codonStart  = $codonNumber * 3;
-            $codon       = substr( $pattern, $codonStart, 3 );
-            if ( $codon =~ /[.nN-]/ ) { next; }
+            my $codonNumber = int( $pos / 3 );
+            my $codonStart  = $codonNumber * 3;
+            my $codon       = substr( $pattern, $codonStart, 3 );
+            if ( $codon =~ /[.nN-]/smx ) { next; }
             $codonData{$codonNumber}{$codon} += $count;
         }
 
     }
 } else {
-    foreach $group ( keys(%counts) ) {
-        @patterns = keys( %{ $counts{$group} } );
+    foreach my $group ( keys(%counts) ) {
+        my @patterns = keys( %{ $counts{$group} } );
         foreach my $pattern (@patterns) {
-            $length = length($pattern);
-            $count  = $counts{$group}{$pattern};
-            for ( $pos = 0; $pos < $length; $pos += 3 ) {
+            my $length = length($pattern);
+            my $count  = $counts{$group}{$pattern};
+
+            ## no critic (ControlStructures::ProhibitCStyleForLoops)
+            for ( my $pos = 0; $pos < $length; $pos += 3 ) {
 
                 # zero based codon number
-                $codonNumber = int( $pos / 3 );
-                $codonStart  = $codonNumber * 3;
-                $codon       = substr( $pattern, $codonStart, 3 );
-                if ( $codon =~ /[.nN-]/ ) { next; }
+                my $codonNumber = int( $pos / 3 );
+                my $codonStart  = $codonNumber * 3;
+                my $codon       = substr( $pattern, $codonStart, 3 );
+                if ( $codon =~ /[.nN-]/smx ) { next; }
                 $codonData{$group}{$codonNumber}{$codon} += $count;
             }
 
